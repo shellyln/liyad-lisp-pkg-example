dacbb188f567dffd1906e1bd0d22c28d5240af11
shellyln <shellyln@users.noreply.github.com>
2018-11-17 22:44:12 +0900

    v0.0.4

--- app.lisp
+++ app.lisp
@@ -3,9 +3,10 @@
 ($let lib ($require "./index"))
 
 
-(::lib:#get @"^/(\?.*)?$" (-> (req res)
+(::lib:#get @"/" (-> (req res)
     ($let u (::url:parse ::req:url))
     (::res@writeHead 200 (# (Content-Type "text/html")))
-    (::res@end ($concat "hit / ," ::req:method "," ::u:path)) ) )
+    (::res@end ($concat "hit / ," ::req:method "," ::u:path)) ))
+
 
 (::lib:serve 3000) ($last "start server")
--- lib/server.lisp
+++ lib/server.lisp
@@ -5,33 +5,30 @@
 
 ($local ((handlers nil)) ($capture (handlers)
 
-    ($defun #get (path handler) ($push handlers ($list "GET" path handler)))
-    ($defun #post (path handler) ($push handlers ($list "POST" path handler)))
-    ($defun #put (path handler) ($push handlers ($list "PUT" path handler)))
+    ($defun #get    (path handler) ($push handlers ($list "GET"    path handler)))
+    ($defun #post   (path handler) ($push handlers ($list "POST"   path handler)))
+    ($defun #put    (path handler) ($push handlers ($list "PUT"    path handler)))
     ($defun #delete (path handler) ($push handlers ($list "DELETE" path handler)))
 
     ($defun serve (port)
         ($let server (::http@createServer (-> (req res)
-            ($let u (::url:parse ::req:url))
-            ($let i 0)
-            ($let x nil)
-            ($let h false)
+            ($let u (::url:parse ::req:url)) ($let i 0) ($let x nil) ($let h false)
             ($while ($and ($not h) (< i ($length handlers)))
                 ($set x ($[ i ] handlers))
-                ($set h ($if ($and (=== ($get x 0) ::req:method) ($match ($get x 1) ::u:path))
+                ($set h ($if ($and (=== ($get x 0) ::req:method) ($match ($concat @"^" ($get x 1) @"(\?.*)?$") ::u:path))
                     ($last (($get x 2) req res) true)
                     false ))
-                ($set i (+ i 1)))
+                ($set i (+ i 1)) )
             ($if h nil
                 ($last
-                    (::res@writeHead 200 (# (Content-Type "text/html")))
-                    (::res@end ($concat "okey," ::req:method "," ::u:path)) )))))
+                    (::res@writeHead 404 (# (Content-Type "text/html")))
+                    (::res@end ($concat "Not found," ::req:method "," ::u:path)) )))))
         (::server@listen port) ) ))
 
 
 ;; exports
-(#  (serve   (<- serve))
-    (#get    (<- #get))
-    (#post   (<- #post))
-    (#put    (<- #put))
+(#  (serve   (<- serve  ))
+    (#get    (<- #get   ))
+    (#post   (<- #post  ))
+    (#put    (<- #put   ))
     (#delete (<- #delete)) )
